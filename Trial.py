47f4181e896483a705a97fbf943fccc1eafed1f0
Fellow3-1 <felixodhiambo31@live.com>
2018-08-07 17:30:42 +0300

    Added Input Functions

--- Trial.py
+++ Trial.py
@@ -1,5 +1,9 @@
 print("Game On Python!!!!!")
 var=2018
-word= "This is a String"
+word= "This is a Simple Calculator"
 print(var)
 print(word)
+print("Enter two digits you want to add")
+a= int(input())
+b= int(input())
+print(a+b)
