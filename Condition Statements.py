09232b02c23cd698e962e2c3f8dd6df69882fe0b
Fellow3-1 <felixodhiambo31@live.com>
2018-08-07 18:50:59 +0300

    Calculator

--- Condition Statements.py
+++ Condition Statements.py
@@ -1,3 +1,7 @@
 x= int(input("Enter The Value Of x"))
 if(x>15):
     print(x)
+elif(x<15):
+    print("Value of x less than 15")
+else:
+    print("Enter a value between higher than 0")
--- Trial.py
+++ Trial.py
@@ -2,7 +2,21 @@
 var=2018
 word= "This is a Simple Calculator"
 print("Game On Python!!!!!", var, word)
-print("Enter two digits you want to add")
+print("Enter two digits you want to Compute")
 a= int(input("Enter Value Of First Digit"))
 b= int(input("Enter Value Of Second Digit"))
-print("The Sum is",a+b)
+op= input("Enter Operator")
+if (op== "+"):
+    print(a+b)
+
+elif (op== "-"):
+    print(a-b)
+
+elif (op== "*"):
+    print(a*b)
+
+elif (op== "/"):
+    print(a/b)
+
+else:
+    print("Invalid Operator")
